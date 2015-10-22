42d0434448d3f9c01b5b5e55ff7c518ffea97dc5
Dafeng Guo <dafeng@strikingly.com>
2015-10-22 23:40:49 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# webpack-bundle-loader-sample
+Testing CommonsChunkPlugin with bundle-loader.
+
+I have a.js and b.js both sharing React lib. I'm trying to use CommonsChunkPlugin to extract React lib out so when requesting, it will async fire three requests:
+
+* react as the common lib
+* a.js
+* b.js
+
+currently, both a.js and b.js have React lib in it, which is not the case I want.
--- main.jsx
+++ main.jsx
@@ -1,4 +1,9 @@
-require(['bundle!./modules/a.js', 'bundle!./modules/b.js'], function(aBundle, bBundle) {
+require(['bundle!./modules/a.js'], function(aBundle, bBundle) {
+	console.log(aBundle((f) => { f.hello() } ))
+	console.log(bBundle((f) => { f.hello() } ))
+})
+
+require(['bundle!./modules/b.js'], function(aBundle, bBundle) {
 	console.log(aBundle((f) => { f.hello() } ))
 	console.log(bBundle((f) => { f.hello() } ))
 })
